c2d1884aadece129e7970de240ce4d503000a6fc
tomballgithub <jeoff@krontz.com>
2017-11-04 23:59:13 -0500

    Fix missing space on scout screen when proxies used

--- pgscout/console.py
+++ pgscout/console.py
@@ -120,7 +120,7 @@ def print_scouts(lines, state, scouts):
                               map(lambda s: len(s.acc.username), scouts)))
     len_num = str(len(str(len(scouts))))
     if cfg_get('proxies'):
-        line_tmpl = u'{:' + len_num + '} | {:' + len_username + '} | {:25} | {:8} | {:4} | {:6} | {:10} | {:5} | {:6} |{:8} | {}'
+        line_tmpl = u'{:' + len_num + '} | {:' + len_username + '} | {:25} | {:8} | {:4} | {:6} | {:10} | {:5} | {:6} | {:8} | {}'
         lines.append(
             line_tmpl.format('#', 'Scout', 'Proxy', 'Start', 'Warn', 'Active', 'Encounters', 'Enc/h', 'Errors',
                              'Last Enc', 'Message'))
