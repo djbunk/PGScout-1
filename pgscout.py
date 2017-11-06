e0128fe739bff3e4f338aa5e40d9cd299ad47418
tomballgithub <jeoff@krontz.com>
2017-11-05 23:20:12 -0600

    Convert force to int before compare

--- pgscout.py
+++ pgscout.py
@@ -46,7 +46,7 @@ def get_iv():
     pokemon_name = get_pokemon_name(pokemon_id)
 
     force = request.args.get("force", 0)
-    if (force == 0):
+    if (int(force) != 1):
         if (any(poke[0] == int(pokemon_id) for poke in blacklist)):
             odds = int(blacklist[[x[0] for x in blacklist].index(int(pokemon_id))][1])
             if (randint (1,100) <= odds):
